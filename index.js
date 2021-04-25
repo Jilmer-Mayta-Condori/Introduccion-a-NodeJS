eee1fa3e9dbfbbcded932dca5b4ac962bd794b7a
Jilmer <jilmer.mayta@tecsup.edu.pe>
2021-04-25 18:22:44 -0500

    5. insert POST

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const app = express()
+const bodyParser = require('body-parser');
 
 var fecha= new Date();
 
@@ -27,7 +28,9 @@ let persons = [
 ]
 
 app.get('/',(request,response) => {
-  response.send('<h1>Bienvenidos a mi pagina con NODEJS</h1>')
+  response.send('<h1>Bienvenidos a mi pagina con NODEJS</h1>'
+  + '<br/><a href="/form"><button>agregar nuevos registros</button></a> <br/>'
+  + '<br/><a href="/api/personas"><button>ver registros JSON</button></a>')
 })
 
 app.get('/info',(request,response) => {
@@ -55,6 +58,38 @@ app.delete('/api/personas/:id',(request,response) => {
   persons = persons.filter(person => person.id !== id)
 
   response.status(204).end()
+
+})
+
+app.get('/form',(request,response) => {
+  response.send('<h1>Bienvenidos a mi formulario con NODEJS</h1>'
+   + '<form method="POST" action="/api/personas">'
+   + '  <label>Nombre</label><br/>'
+   + '    <input type="text" name="name"> <br/><br/>'
+   + '  <label>Telefono</label><br/>'
+   + '    <input type="text" name="number"> <br/><br/>'
+   + '  <button type="submit">Enviar</button>'
+   + '</form>')
+})
+
+app.use(express.json())
+
+//extended: false significa que parsea solo string (no archivos de imagenes por ejemplo)
+app.use(bodyParser.urlencoded({ extended: false }));
+
+app.post('/api/personas',(request,response) => {
+  const nombre = request.body.name
+  const numero = request.body.number
+  const id = Math.floor(Math.random() * (10000 - 1) + 10000)
+
+  const newPerson = {
+    id: id,
+    name: nombre,
+    number: numero
+  }
+  persons = [... persons, newPerson]
+
+  response.redirect('/api/personas')
 
 })
 
